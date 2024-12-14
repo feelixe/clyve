0009f84406a5caf64df937ce8a5711a13fca240e
Felix Eklöf <60542867+feelixe@users.noreply.github.com>
2024-12-14 22:29:05 +0100

    feat/adapter (#1)
    * feat: add adapters
    
    * docs: update readme with adapter creation
    
    * feat: add filesystem adapter
    
    * feat: add example
    
    * feat: add fs adapter example
    
    * ci: add build workflow

---
+++ apps/example/src/fs-adapter.ts
@@ -0,0 +1,34 @@
+import { S3Client } from "@aws-sdk/client-s3";
+import { createClient } from "clyve";
+import { FileSystemAdapter } from "clyve/adapters";
+import "dotenv/config";
+
+type MySchema = {
+  users: {
+    id: string;
+    name: string;
+  };
+  products: {
+    id: string;
+    name: string;
+    price: number;
+  };
+};
+
+export const s3Client = new S3Client({
+  endpoint: process.env.S3_ENDPOINT!,
+  region: process.env.S3_REGION!,
+  credentials: {
+    accessKeyId: process.env.S3_ACCESS_KEY!,
+    secretAccessKey: process.env.S3_SECRET_KEY!,
+  },
+});
+
+const adapter = new FileSystemAdapter("./data");
+const db = createClient<MySchema>(adapter);
+
+await db.users.deleteAll();
+
+const hej = await db.users.all();
+
+console.log(hej);
